d75bd2bb75f3f7f707d53e540acfa0b4b4b42410
Ching <94354399+Chinh00@users.noreply.github.com>
2024-05-19 13:20:52 +0700

    Update NewsFragment.kt

--- src/client/mobile/app/src/main/java/com/superman/movieticket/ui/news/NewsFragment.kt
+++ src/client/mobile/app/src/main/java/com/superman/movieticket/ui/news/NewsFragment.kt
@@ -1,5 +1,15 @@
 package com.superman.movieticket.ui.news
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import com.superman.movieticket.core.view.FragmentBase
 import com.superman.movieticket.databinding.FragmentNewsBinding
 import com.superman.movieticket.ui.news.control.NewsFragmentImpl
@@ -15,6 +25,34 @@ class NewsFragment : FragmentBase<FragmentNewsBinding, NewsFragmentImpl>() {
     }
 
     override fun setupActions() {
+
+
+
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                AppBarCustom(onClick = {
+                    Toast.makeText(requireContext(), "Ching dep trai", Toast.LENGTH_LONG).show()
+                })
+            }
+        }
+    }
+
+
+}
+@Preview
+@Composable
+fun AppBarCustom(onClick: () -> Unit) {
+    return Button(
+            onClick = onClick,
+           
+        ) {
+            Text(text = "An vao day")
+        }
 }
