1c6c5e35b41535a675326d8825269891a7898bbf
ngockhanh0106 <139062369+ngockhanh0106@users.noreply.github.com>
2024-05-27 21:01:28 +0700

    Update EditActivity.kt

--- src/client/mobile/app/src/main/java/com/superman/movieticket/ui/profile/EditActivity.kt
+++ src/client/mobile/app/src/main/java/com/superman/movieticket/ui/profile/EditActivity.kt
@@ -1,19 +1,8 @@
 package com.superman.movieticket.ui.profile
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.os.Build
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -24,58 +13,35 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Done
 import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.superman.movieticket.R
-import com.superman.movieticket.ui.DetailsActivity
-import com.superman.movieticket.ui.favourite.FavouriteItem
-import com.superman.movieticket.ui.favourite.FavouriteScreen
 import com.superman.movieticket.ui.home.model.Movie
 import com.superman.movieticket.ui.home.model.listMovies
-import java.time.LocalDate
-import java.time.format.TextStyle
-import java.util.Locale
-import javax.annotation.meta.When
 
 @Composable
 fun EditActivity() {
@@ -117,7 +83,7 @@ fun EditActivity() {
 
             LazyColumn {
                 item {
-                    Avatar(context, listMovies.first())
+                    Avatar(listMovies.first())
                 }
             }
 
@@ -271,7 +237,7 @@ fun EditActivity() {
 }
 
 @Composable
-fun Avatar(context: Context, movie: Movie) {
+fun Avatar(movie: Movie) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
