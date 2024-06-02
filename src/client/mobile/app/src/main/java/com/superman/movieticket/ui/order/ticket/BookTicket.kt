ba4320939a96f99163e5adcef3eabaafc8672f20
Nguyễn Quang Đông <101628525+Quangdong2k3@users.noreply.github.com>
2024-06-02 08:55:17 +0700

    Update BookTicket.kt

--- src/client/mobile/app/src/main/java/com/superman/movieticket/ui/order/ticket/BookTicket.kt
+++ src/client/mobile/app/src/main/java/com/superman/movieticket/ui/order/ticket/BookTicket.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +25,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -30,19 +34,23 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,6 +74,7 @@ class TicketBookActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TicketActivityComp() {
@@ -127,54 +136,91 @@ fun TicketActivityComp() {
 
             ScreenShape()
         }
-        Column(modifier = Modifier
-            .padding(vertical = 10.dp)
+        var scale by remember {
+            mutableStateOf(1f)
+        }
+        var offSet by remember {
+            mutableStateOf(Offset.Zero)
+        }
+
+        BoxWithConstraints(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
             .constrainAs(t) {
                 top.linkTo(e.bottom)
+                bottom.linkTo(b.top)
 
+            }) {
+            val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+                scale = (scale * zoomChange).coerceIn(1f, 1.5f)
+                val extraWidth = (scale - 1) * constraints.maxWidth
+                val extraHeight = (scale - 1) * constraints.maxHeight
+                val maxX = extraWidth / 2
+                val maxY = extraHeight / 2
+
+                offSet = Offset(
+                    x = (offSet.x + scale * panChange.x).coerceIn(-maxX, maxX),
+                    y = (offSet.y + scale * panChange.y).coerceIn(-maxY, maxY)
+                )
             }
-            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            //Seat Mapping
-            (1..11).forEach { i ->
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    (1..8).forEach { j ->
-                        val seatNumber = "${(64 + i).toChar()}$j"
-                        val isBooked = bookedSeats.contains(seatNumber)
-                        if ( j == 1) {
-                            Row {
-                                Text(
-                                    text = "${(64 + i).toChar()}",
-                                    style = MaterialTheme.typography.titleSmall,
-                                    fontWeight = FontWeight.Bold,
-                                    color = MaterialTheme.colorScheme.background,modifier=Modifier.padding(end = 15.dp)
-                                )
-                            }
-                        }
-                        SeatComp(
-                            seatNumber = seatNumber,
-                            isEnable = !isBooked,
-                            isSelected = selectedSeat.contains(seatNumber)
-                        ) { selected, seat ->
-                            if (selected) selectedSeat.remove(seat)
-                            else selectedSeat.add(seat)
-                        }
-                        if ( j == 8) {
-                            Row {
-                                Text(
-                                    text = "${(64 + i).toChar()}",
-                                    style = MaterialTheme.typography.titleSmall,
-                                    fontWeight = FontWeight.Bold,
-                                    color = MaterialTheme.colorScheme.background,modifier=Modifier.padding(start = 15.dp)
-                                )
-                            }
-                        }
-                        if (j != 8) Spacer(modifier = Modifier.width(if (j == 4) 25.dp else 8.dp))
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                        translationX = offSet.x
+                        translationY = offSet.y
 
                     }
+                    .transformable(state), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                //Seat Mapping
+                (1..11).forEach { i ->
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        (1..8).forEach { j ->
+                            val seatNumber = "${(64 + i).toChar()}$j"
+                            val isBooked = bookedSeats.contains(seatNumber)
+                            if (j == 1) {
+                                Row {
+                                    Text(
+                                        text = "${(64 + i).toChar()}",
+                                        style = MaterialTheme.typography.titleSmall,
+                                        fontWeight = FontWeight.Bold,
+                                        color = MaterialTheme.colorScheme.background,
+                                        modifier = Modifier.padding(end = 15.dp)
+                                    )
+                                }
+                            }
+                            SeatComp(
+                                seatNumber = seatNumber,
+                                isEnable = !isBooked,
+                                isSelected = selectedSeat.contains(seatNumber)
+                            ) { selected, seat ->
+                                if (selected) selectedSeat.remove(seat)
+                                else selectedSeat.add(seat)
+                            }
+                            if (j == 8) {
+                                Row {
+                                    Text(
+                                        text = "${(64 + i).toChar()}",
+                                        style = MaterialTheme.typography.titleSmall,
+                                        fontWeight = FontWeight.Bold,
+                                        color = MaterialTheme.colorScheme.background,
+                                        modifier = Modifier.padding(start = 15.dp)
+                                    )
+                                }
+                            }
+                            if (j != 8) Spacer(modifier = Modifier.width(if (j == 4) 25.dp else 8.dp))
+
+                        }
+
+                    }
+                    Spacer(modifier = Modifier.height(if (i != 6) 8.dp else 25.dp))
 
                 }
-                Spacer(modifier = Modifier.height(if (i != 6) 8.dp else 25.dp))
-
             }
         }
         Row(modifier = Modifier
