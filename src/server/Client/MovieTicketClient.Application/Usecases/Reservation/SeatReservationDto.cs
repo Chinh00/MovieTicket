8f1064ed7f813872cddfca97e1a405eefe7ff9ba
Ching <94354399+Chinh00@users.noreply.github.com>
2024-06-25 18:16:12 +0700

    Update SeatReservationDto.cs

--- src/server/Client/MovieTicketClient.Application/Usecases/Reservation/SeatReservationDto.cs
+++ src/server/Client/MovieTicketClient.Application/Usecases/Reservation/SeatReservationDto.cs
@@ -1,8 +1,14 @@
+using MovieTicketClient.Application.Usecases.Room;
+
 namespace MovieTicketClient.Application.Usecases.Reservation;
 
 public class SeatReservationDto
 {
     public Guid SeatId { get; init; }
-    public string RowNumber { get; set; }
-    public string ColNumber { get; set; }
+    public virtual SeatDto Seat { get; set; }
+    
+    public Guid? ReservationId { get; set; }
+    
+    public virtual ReservationDto Reservation { get; set; }
+    
 }
